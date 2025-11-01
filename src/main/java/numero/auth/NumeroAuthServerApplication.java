5e1c0fc20131d55fcd2d0d0b978c8de442ac88a8
unknown <csenggsahilgupta@gmail.com>
2025-11-01 19:33:43 +0530

    auth server initial commit

---
+++ src/main/java/numero/auth/NumeroAuthServerApplication.java
@@ -0,0 +1,13 @@
+package numero.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumeroAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumeroAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/numero/auth/config/NumroAuth.java
@@ -0,0 +1,105 @@
+package numero.auth.config;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class NumroAuth {
+
+	@Bean
+	@Order(1)
+	SecurityFilterChain	 AuthServerSecurityFilterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults()).registeredClientRepository(registeredClientRepository());
+		return http.sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+
+
+	}
+	
+	@Order(2)
+	@Bean
+	SecurityFilterChain	 DefaultSecurityFilterChain(HttpSecurity http) throws Exception {
+http.authorizeHttpRequests(a->a.anyRequest().authenticated()).formLogin(Customizer.withDefaults()).csrf(c->c.disable()).cors(c->c.disable()) ;
+return http.build();
+		
+		
+		
+		
+
+	}
+
+
+    @Bean
+    UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.withUsername("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // This is for demonstration purposes only and should not be used in production.
+        // It provides no password encoding and is highly insecure.
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+
+    @Bean
+    RegisteredClientRepository registeredClientRepository() {
+		RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("numero-client")
+				.clientSecret("numer-rpaks")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.redirectUri("http://127.0.0.1:8080")
+//				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+            	.scope("openid")
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
+				.build();
+
+		return new InMemoryRegisteredClientRepository(oidcClient);
+	}	
+	
+	
+	
+	
+	
+//	
+//	
+	
+	
+	
+	
+	
+}
