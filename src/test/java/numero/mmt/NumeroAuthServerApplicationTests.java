498b166d7f7df13e344f4040da38f41534d3132f
unknown <csenggsahilgupta@gmail.com>
2025-11-01 20:02:57 +0530

    update gitignore

--- src/test/java/numero/mmt/NumeroAuthServerApplicationTests.java
+++ src/test/java/numero/mmt/NumeroAuthServerApplicationTests.java
@@ -1,13 +1,13 @@
-package numero.mmt;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NumeroAuthServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package numero.mmt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NumeroAuthServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
