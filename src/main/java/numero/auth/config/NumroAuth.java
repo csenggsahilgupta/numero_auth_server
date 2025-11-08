260ea88c44fb2906754ad663bf73c2778a7b47f1
unknown <csenggsahilgupta@gmail.com>
2025-11-08 17:01:10 +0530

    commit

--- src/main/java/numero/auth/config/NumroAuth.java
+++ src/main/java/numero/auth/config/NumroAuth.java
@@ -3,6 +3,7 @@ package numero.auth.config;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
@@ -27,9 +28,14 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin
 public class NumroAuth {
 
 	@Bean
@@ -37,15 +43,27 @@ public class NumroAuth {
 	SecurityFilterChain	 AuthServerSecurityFilterChain(HttpSecurity http) throws Exception {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults()).registeredClientRepository(registeredClientRepository());
-		return http.sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+		return http.sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).csrf(csrf->csrf.disable()).build();
 
 
 	}
-	
+//	@Bean
+//	 CorsConfigurationSource corsCoongigration() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//
+//		CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true); // Allow sending cookies and authentication headers
+//        config.setAllowedOrigins(List.of("localhost:4201")); // Specify allowed origins
+//        config.setAllowedHeaders(List.of("*")); // Allow all headers
+//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow specified HTTP methods
+//        source.registerCorsConfiguration("/oauth/token", config); // A	// TODO Auto-generated method stub
+//		return source;
+//	}
+
 	@Order(2)
 	@Bean
 	SecurityFilterChain	 DefaultSecurityFilterChain(HttpSecurity http) throws Exception {
-http.authorizeHttpRequests(a->a.anyRequest().authenticated()).formLogin(Customizer.withDefaults()).csrf(c->c.disable()).cors(c->c.disable()) ;
+http.authorizeHttpRequests(a->a.anyRequest().authenticated()).formLogin(Customizer.withDefaults()).csrf(c->c.disable()) ;
 return http.build();
 		
 		
